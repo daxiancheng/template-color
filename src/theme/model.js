fb2418508b14dfd28fde1b5f9f1f934fe14a71af
chengzi <webchengzi@foxmail.com>
2022-03-24 13:43:34 +0800

    脚手架模板示例

---
+++ src/theme/model.js
@@ -0,0 +1,13 @@
+// 一套默认主题以及一套暗黑主题
+export const themes = {
+    default: {
+      primaryColor: '#fff',
+      primaryTextColor: '#000',
+      aColor: 'red'
+    },
+    dark: {
+      primaryColor: '#000',
+      primaryTextColor: '#fff',
+      aColor: 'green'
+    },
+  };
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
+console.log('process.env', process.env)
+module.exports = defineConfig({
+  transpileDependencies: true,
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "less",
+      patterns: [
+        // 这个是加上自己的路径,不能使用(如下:alias)中配置的别名路径
+        path.resolve(__dirname, "./src/theme/default.less"),
+      ],
+    },
+  },
+})
